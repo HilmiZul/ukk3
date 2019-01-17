78905db77365f5357d101dc7824eae22206b3f90
hilmizul <netspytux@gmail.com>
2019-01-17 15:33:18 +0700

    add action and ...

--- detil_pinjam.php
+++ detil_pinjam.php
@@ -8,10 +8,53 @@
 </head>
 <body>
   <h3>PILIH BARANG</h3>
+  <?php
+  include('koneksi.php');
+  // SAVE.DETIL.PEMINJAMAN
+  if (isset($_POST['simpan'])) {
+    $id_peminjaman = $_POST['id_peminjaman'];
+    $id_inventaris = $_POST['id_inventaris'];
+    $jumlah = $_POST['jumlah'];
+
+    $save = mysqli_query($conn, "insert into detil_pinjam values (
+                        NULL,
+                        $id_inventaris,
+                        $jumlah,
+                        $id_peminjaman
+                        )");
+    if ($save) {
+      print "Data tersimpan";
+    } else {
+      print "error :(";
+    }
+  }
+
+
+  // GET ID_PEMINJAMAN
+  if (isset($_POST['lanjut'])) {
+    $id_pegawai = $_POST['id_pegawai'];
+    mysqli_query($conn, "insert into peminjaman values (
+                        NULL,
+                        NOW(),
+                        NULL,
+                        'sedang pinjam',
+                        $id_pegawai
+                        )");
+    
+    $q = mysqli_query($conn, "select id_peminjaman from peminjaman order by id_peminjaman desc limit 0,1");
+    $row = mysqli_fetch_array($q);
+  }
+  ?>
   <form action="" method="post">
+    <input type="hidden" name="id_peminjaman" value="<?php print $row['id_peminjaman']?>">
     <div>
       <select name="id_inventaris" >
         <option value="">PILIH BARANG</option>
+        <?php 
+        $q_i = mysqli_query($conn, "select * from inventaris");
+        while ($r = mysqli_fetch_array($q_i)) {?>
+          <option value="<?php print $r['id_inventaris']?>"><?php print $r['nama']?></option>
+        <?php }?>
       </select>
     </div>
 
